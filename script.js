12dd356acee66d01f11bde11986be182c6dd8c0b
mshro3e <137133778+mshro3e@users.noreply.github.com>
2023-06-20 07:15:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var intervalID = window.setInterval(updateScreen, 200);
+var c = document.getElementById("console");
+
+var txt = [
+  "FORCE: XX0022. ENCYPT://000.222.2345",
+  "TRYPASS: ********* AUTH CODE: ALPHA GAMMA: 1___ PRIORITY 1",
+  "RETRY: REINDEER FLOTILLA",
+  "Z:> /FALKEN/GAMES/TICTACTOE/ EXECUTE -PLAYERS 0",
+  "================================================",
+  "Priority 1 // local / scanning...",
+  "scanning ports...",
+  "BACKDOOR FOUND (hacker011010)",
+  "BACKDOOR FOUND (hacker is in)",
+  "BACKDOOR FOUND(attacker)",
+  "...",
+  "...",
+  "BRUTE.EXE -r -z",
+  "...locating vulnerabilities...",
+  "...vulnerabilities found...",
+  "MCP/> DEPLOY CLU",
+  "SCAN: __ 0100.0000.0554.0080",
+  "SCAN: __ how ALSAHLI hacked me?",
+  "SCAN: __ hacked by ALSAHLI",
+  "SCAN: __ drive (c)deleted by ALSAHLI",
+  "SCAN: __ your ip address wsp down",
+  "SCAN: __ ALSAHLI turn off the pc",
+ "SCAN: __ ALSAHLI delete the windows",
+
+]
+
+var docfrag = document.createDocumentFragment();
+
+function updateScreen() {
+  //Shuffle the "txt" array
+  txt.push(txt.shift());
+  //Rebuild document fragment
+  txt.forEach(function(e) {
+    var p = document.createElement("p");
+    p.textContent = e;
+    docfrag.appendChild(p);
+  });
+  //Clear DOM body
+  while (c.firstChild) {
+    c.removeChild(c.firstChild);
+  }
+  c.appendChild(docfrag);
+}
